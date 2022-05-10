b5945b4113c9b2537c0b8fff1df3c8cd9ba7bb0b
Saulo-gitlab <reydominguezsaulo7@gmail.com>
2022-05-10 18:13:12 +0200

    Importo componentes máis creo os path no app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { TrabajosComponent } from './trabajos/trabajos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'trabajos', component: TrabajosComponent },
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
